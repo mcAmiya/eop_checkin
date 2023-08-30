8f038848502e6df8fe13144f800282d0a1948dd8
Amiya <mc.amiya@qq.com>
2023-08-30 21:31:18 +0800

    修复： 1.修复连续签到返回连续签到天数误认为签到失败的报错 2.新增连续签到判断并发送连续签到天数

--- main.py
+++ main.py
@@ -14,11 +14,18 @@ headers = {
 
 response = requests.request("POST", url, headers=headers, data=payload)
 
+# 测试
+# class hi ():
+#     pass
+# response = hi
+# response.text = '8'
+
 res = app_config.check_fail + response.text
 if response.text == 'success':
     res = app_config.check_success
+elif response.text.isdigit():
+    res = app_config.check_success + ' 连续签到第%s天' % response.text
 elif response.text == 'already':
     res = app_config.check_already
 print(res)
 pushplus.send(res, title=app_config.pushplus_title)
-
